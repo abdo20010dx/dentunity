581db75958beb642a9707e156a9d6b61bf719c61
abdo20010dx <abdo20010d0@gmail.com>
2022-06-11 11:21:53 +0200

    port changed

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,6 @@ async function bootstrap() {
     whitelist: true, //just get data as dto
     // transform: true // you can  set default values in dto
   }));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000 || process.env.PORT);
 }
 bootstrap();
